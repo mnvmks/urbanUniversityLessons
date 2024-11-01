f648d869cb167b0595bf4a87544fae7f02b1846f
mnvmks <113935921+mnvmks@users.noreply.github.com>
2024-11-02 02:22:36 +0300

    Base Data Structures Lesson

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1st program
+print((9 ** 0.5) * 5)
+
+# 2nd program
+print((9.99 > 9.98) and (1000 != 1000.1))
+
+# 3rd program
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a)
+print(b)
+print(a == b)
+
+# 4th program
+str_a = '123.456'
+str_a = float(str_a) * 10
+str_a = int(str_a) % 10
+print(str_a)
+
