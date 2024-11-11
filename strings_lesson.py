42954499af01fbf18dace74d9d20cf98f0c5952e
Maxim Manyukov <113935921+mnvmks@users.noreply.github.com>
2024-11-12 01:40:36 +0300

    Urban University - strings lesson

---
+++ strings_lesson.py
@@ -0,0 +1,17 @@
+#1
+example = "sample string" #variable assignment
+
+#2
+print(example[0]) #print first char from example to console
+
+#3
+print(example[-1]) #print last char from example to console
+
+#4
+print(example[7:]) #print slice to console
+
+#5
+print(example[::-1]) #reverse print to console
+
+#6
+print(example[1::2]) #print every second char to console
