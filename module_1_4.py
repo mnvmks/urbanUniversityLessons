019578ca3feabe265d2e6fc68177ed59ccad98eb
Maxim Manyukov <113935921+mnvmks@users.noreply.github.com>
2024-11-12 02:08:14 +0300

    UU - organize program & strings methods

---
+++ module_1_4.py
@@ -0,0 +1,12 @@
+# 1 module_1_4.py
+
+# 2 organize program
+my_string = input("Type some text here: ")  # User input text
+print(len(my_string))  # Print length of user text
+
+# 3 string methods
+print(my_string.upper())  # Print user text in upper case
+print(my_string.lower())  # Print user text in lower case
+print(my_string.replace(" ", ""))  # Replase gaps to ""
+print(my_string[0])  # Print first char to console
+print(my_string[-1])  # Print last char to console
